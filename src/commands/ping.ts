b6f9c3292cc7546ef74d1cae2b629e3f907d405c
Sacha Rodier <sacha_rodier@ymail.com>
2021-05-04 23:44:11 +0200

    adding first aliases

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,10 +1,12 @@
-import { Command, CommandMessage, Guard } from '@typeit/discord';
+import { Command, CommandMessage, Guard, Rule, Rules } from '@typeit/discord';
 import { NotBot } from '../guards/not-bot';
 import { AbuelaCommand } from '../types';
 
 export abstract class Ping implements AbuelaCommand {
 
-  @Command('ping')
+  @Command()
+  @Rules(Rule('ping'))
+  @Rules(Rule('test'))
   @Guard(NotBot)
   async execute(command: CommandMessage) {
     await command.channel.send('pong');
