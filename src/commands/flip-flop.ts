f2345768ae13d802d4e0c036504bf37ddf2bd833
Sacha Rodier <sacha_rodier@ymail.com>
2021-05-05 12:22:11 +0200

    optimizing aliases decorator

--- src/commands/flip-flop.ts
+++ src/commands/flip-flop.ts
@@ -1,14 +1,15 @@
-import { Command, CommandMessage, Guard, Rule, Rules } from '@typeit/discord';
+import { Command, CommandMessage, Guard } from '@typeit/discord';
 import { NotBot } from '../guards/not-bot';
 import { AbuelaCommand } from '../types';
 import { Aliases } from '../decorators/aliases';
 
 export abstract class FlipFlop implements AbuelaCommand {
 
-  @Command('flip')
-  @Aliases('flop', 'flup')
+  @Command('flip :slug :number')
+  @Aliases('flop :slug :number', 'flup :slug :number')
   @Guard(NotBot)
   async execute(command: CommandMessage) {
+    const { slug, number } = command.args;
     await command.channel.send('Testing Aliases');
   }
 }
