76247b74fca7d712231e9e4b9086ae41c90ad539
Monsieur-Monet <timoscheer11@gmail.com>
2021-05-12 17:49:31 +0200

    Added the "Richard Rolle" command

---
+++ src/commands/richard-rolle.command.ts
@@ -0,0 +1,21 @@
+import { Command, CommandMessage, Guard, Infos } from '@typeit/discord';
+import { NotBotGuard } from '../guards/not-bot.guard';
+import { AbuelaCommand, AbuelaCommandInfos } from '../types';
+import { NotHelpGuard } from '../guards/not-help.guard';
+import { Aliases } from '../decorators/aliases';
+
+export abstract class RichardRolleCommand implements AbuelaCommand {
+  private static readonly infos: AbuelaCommandInfos = {
+    description: 'This command is very usefull.',
+    usage: 'type "richardrolle" and see magic happen :sparkles:',
+    aliases: ['rolle','richard','rr']
+  };
+
+  @Command('richardrolle')
+  @Infos(RichardRolleCommand.infos)
+  @Guard(NotHelpGuard, NotBotGuard)
+  @Aliases(RichardRolleCommand.infos.aliases!)
+  async execute(command: CommandMessage) {
+    await command.channel.send('https://tenor.com/view/rick-ashtley-never-gonna-give-up-rick-roll-gif-4819894');
+  }
+}
