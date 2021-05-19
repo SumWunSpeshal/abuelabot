3790ca7693e570683cc17f47b066dbad472469df
Sacha Rodier <sacha_rodier@ymail.com>
2021-05-19 15:00:32 +0200

    adding aliases for ping

--- src/commands/ping.command.ts
+++ src/commands/ping.command.ts
@@ -2,18 +2,20 @@ import { Command, CommandMessage, Guard, Infos } from '@typeit/discord';
 import { NotBotGuard } from '../guards/not-bot.guard';
 import { AbuelaCommand, AbuelaCommandInfos } from '../types';
 import { NotHelpGuard } from '../guards/not-help.guard';
+import { Aliases } from '../decorators/aliases';
 
 const INFOS: AbuelaCommandInfos = {
   commandName: 'ping',
   description: `The "Hello World" of all commands. Just make me respond with "pong". Useful to see whether I'm alive or not.`,
   usage: '`!ping`',
-  aliases: []
+  aliases: ['test', 'echo']
 };
 
 export abstract class PingCommand implements AbuelaCommand {
 
   @Command(INFOS.commandName)
   @Infos(INFOS)
+  @Aliases(INFOS.aliases)
   @Guard(NotHelpGuard, NotBotGuard)
   async execute(command: CommandMessage) {
     await command.channel.send('pong');
