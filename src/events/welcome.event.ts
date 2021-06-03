df1bb55f6222b37d93f3ee798c72ebc59e324390
Sacha Rodier <sacha_rodier@ymail.com>
2021-06-03 22:24:39 +0200

    tweaking welcome event

--- src/events/welcome.event.ts
+++ src/events/welcome.event.ts
@@ -3,7 +3,7 @@ import { AbuelaEvent } from '../types';
 import { TextChannel } from 'discord.js';
 import { CommandHelper } from '../utils/command-helper';
 
-const WELCOME_MESSAGE = `Welcome %USERNAME%. We hope you'll have a great stay. \n\nIf you want to learn more about me, just type \`/\` in any of this server's text channels and see all the cool moves I know.`;
+const WELCOME_MESSAGE = `Welcome %USER_NAME% to %GUILD_NAME%. We hope you'll have a great stay. \n\nIf you want to learn more about me, just type \`/\` in any of this server's text channels and see all the cool moves I know.`;
 
 @Discord()
 export abstract class WelcomeEvent implements AbuelaEvent {
@@ -14,7 +14,11 @@ export abstract class WelcomeEvent implements AbuelaEvent {
     }) as TextChannel | undefined;
 
     if (defaultTextChannel) {
-      await defaultTextChannel.send(WELCOME_MESSAGE.replace('%USERNAME%', CommandHelper.mentionMember(member.user.id)));
+      await defaultTextChannel.send(
+        WELCOME_MESSAGE
+          .replace('%USER_NAME%', CommandHelper.mentionMember(member.user.id))
+          .replace('%GUILD_NAME%', `\`${member.guild.name}\``)
+      );
     }
   }
 }
