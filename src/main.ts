64fac643bad754464e455fb2e936064c4830f8c8
Sacha Rodier <sacha_rodier@ymail.com>
2021-06-04 11:18:39 +0200

    revert

--- src/main.ts
+++ src/main.ts
@@ -43,8 +43,8 @@ export class Main {
     });
 
     this.initCronJobs();
-    // await this.clearSlashes();
-    // await this.initSlashes();
+    await this.clearSlashes();
+    await this.initSlashes();
     this.initOnInteractionEvent();
     console.info(`### ${this._client.user?.username} ready! ###`);
   }
