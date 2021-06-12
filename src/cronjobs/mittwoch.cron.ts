a5c2288206f459949b9ac4bb4f1209e160662f13
Sacha Rodier <sacha_rodier@ymail.com>
2021-06-12 16:38:57 +0200

    test commit

--- src/cronjobs/mittwoch.cron.ts
+++ src/cronjobs/mittwoch.cron.ts
@@ -14,6 +14,7 @@ export const mittwochCron = schedule(rule, async () => {
     return;
   }
 
+
   const numOfFiles = readdirSync(Path.join(__dirname, '..', 'assets', 'img', 'mittwoch')).length - 1;
   const randomNum = Random.getRandomNumBetween(1, numOfFiles);
   const img = Path.join(__dirname, '..', 'assets', 'img', 'mittwoch', `${randomNum}.jpg`);
