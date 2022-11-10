03c63d94eb7b6694cb62d1763af5445b9a6715c2
Sacha Rodier <mail@srodier.com>
2022-11-10 11:44:51 +0100

    removes amongUsThursdayCron

--- src/cronjobs/index.ts
+++ src/cronjobs/index.ts
@@ -1,4 +1,7 @@
 import { mittwochCron } from './mittwoch.cron';
-import { amongUsThursdayCron } from './among-us-thursday.cron';
+// import { amongUsThursdayCron } from './among-us-thursday.cron';
 
-export const cronJobs = [mittwochCron, amongUsThursdayCron];
+export const cronJobs = [
+    mittwochCron, 
+    // amongUsThursdayCron
+];
